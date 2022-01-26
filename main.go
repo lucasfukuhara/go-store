5f5bbaeaea97d76438eb787723b5e38b8c4c6f1a
Lucas Fukuhara <62750796+lucasfukuhara@users.noreply.github.com>
2022-01-26 09:53:06 -0300

    Adding product list

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
 var templateList = template.Must(template.ParseGlob("templates/*.html")) // load all files with .html from templates folder
 
 func main() {
@@ -13,9 +20,17 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	//creating a slice of products to send to index
+	products := []Product{
+		{Name: "T-shirt", Description: "The Withcer 3", Price: 30.50, Quantity: 10},
+		{Name: "T-shirt", Description: "Blue with white lines", Price: 40.50, Quantity: 5},
+		{"Tenis", "Confortable", 89.99, 3},
+		{"Headset", "Very good", 49.99, 5},
+	}
+
 	/* will send the template index as response, the last parameter is to send
 	some information to the template.
 	The Index is the anotation {{define}} we added on index.html
 	*/
-	templateList.ExecuteTemplate(w, "Index", nil)
+	templateList.ExecuteTemplate(w, "Index", products)
 }
