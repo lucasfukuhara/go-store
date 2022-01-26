9db70389e4148c840120f673f6b2f4b9ce701bc1
Lucas Fukuhara <62750796+lucasfukuhara@users.noreply.github.com>
2022-01-26 10:21:36 -0300

    adding db connection

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
+
+// godoc.org is a repository to check packages we can use
+
+func dbConnect() *sql.DB {
+	connection := "user=postgres dbname=so_store password=admin host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err.Error())
+	} else {
+		return db
+	}
+}
 
 type Product struct {
 	Name        string
@@ -15,6 +30,10 @@ type Product struct {
 var templateList = template.Must(template.ParseGlob("templates/*.html")) // load all files with .html from templates folder
 
 func main() {
+
+	db := dbConnect()
+	defer db.Close()
+
 	http.HandleFunc("/", index)       // will link the address / with the func index
 	http.ListenAndServe(":8000", nil) //start the server on port 8000
 }
@@ -24,7 +43,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	products := []Product{
 		{Name: "T-shirt", Description: "The Withcer 3", Price: 30.50, Quantity: 10},
 		{Name: "T-shirt", Description: "Blue with white lines", Price: 40.50, Quantity: 5},
-		{"Tenis", "Confortable", 89.99, 3},
+		{"Sneakers", "Confortable", 89.99, 3},
 		{"Headset", "Very good", 49.99, 5},
 	}
 
